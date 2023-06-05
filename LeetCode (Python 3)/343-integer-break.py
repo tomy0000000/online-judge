4aee8a6fd6850a629ef2d09e84b49eb171931aff
Tomy Hsieh <git@tomy.me>
2023-06-05 21:58:31 +0800

    343. Integer Break

---
+++ LeetCode (Python 3)/343-integer-break.py
@@ -0,0 +1,15 @@
+class Solution:
+    def integerBreak(self, n: int) -> int:
+        
+        if n == 2:
+            return 1
+        if n == 3:
+            return 2
+
+        rest = n
+        num_of_threes = 0
+        while rest > 4:
+            rest -= 3
+            num_of_threes += 1
+
+        return 3 ** num_of_threes * rest
