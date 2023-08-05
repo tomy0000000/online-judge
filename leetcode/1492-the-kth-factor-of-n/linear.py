b463dfec08fdf609fb57a6b7dcbb130990724f62
Tomy Hsieh <git@tomy.me>
2023-08-05 10:08:53 -0400

    LeetCode: 1492

---
+++ leetcode/1492-the-kth-factor-of-n/linear.py
@@ -0,0 +1,13 @@
+# Time: O(n)
+# Space: O(1)
+
+
+class Solution:
+    def kthFactor(self, n: int, k: int) -> int:
+        for i in range(1, n + 1):
+            if n % i == 0:
+                k -= 1
+            if not k:
+                return i
+
+        return -1
