80db766d5a0d6cdcb1536e919e9eaca4c563c73c
Tomy Hsieh <git@tomy.me>
2023-08-28 22:35:22 -0400

    LeetCode: 7 (Migrate)

--- leetcode/7-reverse-integer/main.py
+++ leetcode/7-reverse-integer/main.py
@@ -1,9 +1,9 @@
+# Time: O(1)
+# Space: O(1)
+
+
 class Solution:
-    def reverse(self, x):
-        """
-        :type x: int
-        :rtype: int
-        """
+    def reverse(self, x: int) -> int:
         if x >= 0:
             ans = int("".join(list(str(x))[::-1]))
             return 0 if ans > 2147483647 else ans
