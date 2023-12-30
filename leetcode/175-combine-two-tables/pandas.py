b6b67db19a67c7ad0a0a35d2e8977df062a03180
Tomy Hsieh <git@tomy.me>
2023-12-31 01:24:05 +0800

    LeetCode: 175

---
+++ leetcode/175-combine-two-tables/pandas.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+
+def combine_two_tables(person: pd.DataFrame, address: pd.DataFrame) -> pd.DataFrame:
+    return pd.merge(person, address, how="left", on="personId")[
+        ["firstName", "lastName", "city", "state"]
+    ]
