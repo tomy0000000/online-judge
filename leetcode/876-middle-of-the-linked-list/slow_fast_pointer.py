e86c85df39d70e77cb63f255e08fdeffe6f8c40a
Tomy Hsieh <git@tomy.me>
2024-01-14 08:09:36 -0500

    LeetCode: 876

---
+++ leetcode/876-middle-of-the-linked-list/slow_fast_pointer.py
@@ -0,0 +1,22 @@
+# Let n be the length of the linked list.
+#
+# Time: O(n)
+# Space: O(1)
+
+from typing import Optional
+
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        slow, fast = head, head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        return slow
