5ad44734136f30132b74e3152c9634c9846a2902
Tomy Hsieh <git@tomy.me>
2024-05-31 22:06:44 -0700

    LeetCode-3110

---
+++ leetcode/3110-score-of-a-string/linear.py
@@ -0,0 +1,9 @@
+# Let n be the length of s
+#
+# Time: O(n)
+# Space: O(1)
+
+
+class Solution:
+    def scoreOfString(self, s: str) -> int:
+        return sum(abs(ord(s[i]) - ord(s[i - 1])) for i in range(1, len(s)))
