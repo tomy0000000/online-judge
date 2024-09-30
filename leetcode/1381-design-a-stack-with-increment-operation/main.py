470aaed3c9c555e66f851eca5af8c2e880681ee8
Tomy Hsieh <git@tomy.me>
2024-09-30 01:43:31 -0400

    LeetCode-1381

---
+++ leetcode/1381-design-a-stack-with-increment-operation/main.py
@@ -0,0 +1,17 @@
+class CustomStack:
+    def __init__(self, maxSize: int):
+        self.stack = []
+        self.max_size = maxSize
+
+    def push(self, x: int) -> None:
+        if len(self.stack) < self.max_size:
+            self.stack.append(x)
+
+    def pop(self) -> int:
+        if self.stack:
+            return self.stack.pop()
+        return -1
+
+    def increment(self, k: int, val: int) -> None:
+        for i in range(min(k, len(self.stack))):
+            self.stack[i] += val
