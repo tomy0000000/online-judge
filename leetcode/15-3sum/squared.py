9784a0e1958cc399f29e9efc0c03bda37a2b7617
Tomy Hsieh <git@tomy.me>
2024-10-08 05:57:19 -0400

    LeetCode-15 update

--- leetcode/15-3sum/squared.py
+++ leetcode/15-3sum/squared.py
@@ -6,22 +6,18 @@
 
 class Solution:
     def threeSum(self, nums: list[int]) -> list[list[int]]:
-        # This problem is has a lower bound of O(n^2)
-        # so it's fine to sort the nums first
         nums.sort()
 
-        ans = []
-        # One loop to iterate through the nums
+        ans = set()
         for i in range(len(nums)):
             left = i + 1
             right = len(nums) - 1
-            # Two pointers to find the other two numbers in one loop
             while left < right:
                 total = nums[i] + nums[left] + nums[right]
                 if total == 0:
-                    triplet = [nums[i], nums[left], nums[right]]
+                    triplet = (nums[i], nums[left], nums[right])
                     if triplet not in ans:
-                        ans.append(triplet)
+                        ans.add(triplet)
                 if total <= 0:
                     left += 1
                 if total >= 0:
