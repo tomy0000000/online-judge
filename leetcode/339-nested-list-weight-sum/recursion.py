e46a0eaf476027a3b709899781f6edea572e4812
Tomy Hsieh <git@tomy.me>
2024-11-10 18:26:40 -0500

    LeetCode-339

---
+++ leetcode/339-nested-list-weight-sum/recursion.py
@@ -0,0 +1,56 @@
+# Let n be the total number of elements in nestedList
+#
+# Time: O(n)
+# Space: O(1)
+
+
+class NestedInteger:
+    def __init__(self, value=None):
+        """
+        If value is not specified, initializes an empty list.
+        Otherwise initializes a single integer equal to value.
+        """
+
+    def isInteger(self):
+        """
+        @return True if this NestedInteger holds a single integer, rather than a nested list.
+        :rtype bool
+        """
+
+    def add(self, elem):
+        """
+        Set this NestedInteger to hold a nested list and adds a nested integer elem to it.
+        :rtype void
+        """
+
+    def setInteger(self, value):
+        """
+        Set this NestedInteger to hold a single integer equal to value.
+        :rtype void
+        """
+
+    def getInteger(self):
+        """
+        @return the single integer that this NestedInteger holds, if it holds a single integer
+        Return None if this NestedInteger holds a nested list
+        :rtype int
+        """
+
+    def getList(self):
+        """
+        @return the nested list that this NestedInteger holds, if it holds a nested list
+        Return None if this NestedInteger holds a single integer
+        :rtype List[NestedInteger]
+        """
+
+
+class Solution:
+    def depthSum(self, nestedList: list[NestedInteger], depth: int = 1) -> int:
+        return sum(
+            (
+                element.getInteger() * depth
+                if element.isInteger()
+                else self.depthSum(element.getList(), depth + 1)
+            )
+            for element in nestedList
+        )
